14d78271e0b5244bf7046b25c5ac3ca5259e7050
Jeet Patel <68679391+Jeetp0619@users.noreply.github.com>
2024-02-19 12:01:40 -0800

    Added all app views (#2)

---
+++ AquaGrowthMobile/Views/plant_view.swift
@@ -0,0 +1,8 @@
+//
+//  plant_view.swift
+//  AquaGrowthMobile
+//
+//  Created by Jeet Patel on 2/19/24.
+//
+
+import Foundation
---
+++ AquaGrowthMobile/Views/profile_view.swift
@@ -0,0 +1,8 @@
+//
+//  profile_view.swift
+//  AquaGrowthMobile
+//
+//  Created by Jeet Patel on 2/19/24.
+//
+
+import Foundation
