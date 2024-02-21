97c98a4aa8a8b736c3783601918832f6475b3a80
Jeet Patel <68679391+Jeetp0619@users.noreply.github.com>
2024-02-21 11:43:54 -0800

    added bluetootj model and bluetootj viewmodel

---
+++ AquaGrowthMobile/Models/bluetooth_model.swift
@@ -0,0 +1,8 @@
+//
+//  bluetooth_model.swift
+//  AquaGrowthMobile
+//
+//  Created by Jeet Patel on 2/21/24.
+//
+
+import Foundation
---
+++ AquaGrowthMobile/bluetooth_viewmodel.swift
@@ -0,0 +1,8 @@
+//
+//  bluetooth_viewmodel.swift
+//  AquaGrowthMobile
+//
+//  Created by Jeet Patel on 2/21/24.
+//
+
+import Foundation
