20c11702ba45164c1d8cd2e93cb28f32b33a3558
Mushmou <noahjacinto03@gmail.com>
2024-02-21 12:00:37 -0800

    removed outline files

---
+++ AquaGrowthMobile/Models/bluetooth_model.swift
@@ -0,0 +1,8 @@
+//
+//  bluetooth_model.swift
+//  AquaGrowthMobile
+//
+//  Created by Noah Jacinto on 2/21/24.
+//
+
+import Foundation
--- AquaGrowthMobile/bluetooth_viewmodel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  bluetooth_viewmodel.swift
-//  AquaGrowthMobile
-//
-//  Created by Jeet Patel on 2/21/24.
-//
-
-import Foundation
