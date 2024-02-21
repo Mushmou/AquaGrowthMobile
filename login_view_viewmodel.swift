634d43dd9635a795100592af2d75992631b99f96
Mushmou <noahjacinto03@gmail.com>
2024-02-21 11:35:36 -0800

    Added Models and Viewmodels

--- login_view_viewmodel.swift
+++ login_view_viewmodel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+
+class LoginViewViewModel: ObservableObject{
+    @Published var email = ""
+    @Published var password = ""
+    @Published var errorMessage = ""
+    
+    init(){}
+    
+    func login(){
+        print("Doing some login")
+    }
+}
+
