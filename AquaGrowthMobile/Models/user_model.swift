435936d80f64dc219be4840f83534aea5f494cdf
Mushmou <noahjacinto03@gmail.com>
2024-03-10 16:02:22 -0700

    Added database

--- AquaGrowthMobile/Models/user_model.swift
+++ AquaGrowthMobile/Models/user_model.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct User: Codable{
+    let id: String
+    let username: String
+    let email: String
+    let password: String
+    let signupDateTime: TimeInterval
+}
