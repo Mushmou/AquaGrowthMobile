b936b989862445239d1e8a6eb87a144de2ad765e
Jeet Patel <68679391+Jeetp0619@users.noreply.github.com>
2024-03-10 11:09:53 -0700

    Update plant_viewmodel.swift
    Added plant Struct to hold values for a plant

--- AquaGrowthMobile/ViewModels/plant_viewmodel.swift
+++ AquaGrowthMobile/ViewModels/plant_viewmodel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Plant: Identifiable {
+    var id = UUID()
+    var plant_name: String
+    var plant_type: String
+    var plant_description: String
+    var plant_image: String // This should be a string referring to the image name
+}
+
+//class PlantManager: ObservableObject{
+//    @Published var plants = [Plant]
+//    
+//}
