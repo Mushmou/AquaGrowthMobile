565ceaf23b74e30bc8510d29b2eafd1a05c6c275
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2024-04-16 20:33:13 -0700

    Added feature for Sign in With Apple (#44)
    * pulled new main
    
    * Added google authentication
    
    * Fixed the account view pushing models down, and connected database to pull general user information
    
    * Made secure and text fields for account settings
    
    * I made the individual plant pages, allowed setting data for plants, and added new images
    
    * Made modifications to plants
    
    * Send edit plant view
    
    * Modified individuals
    
    * Save data
    
    * Add entitlements, registered Jeets device
    
    * Add ignore file
    
    * Delete .DS_Store
    
    * Delete AquaGrowthMobile/.DS_Store
    
    * Remove DS_Store
    
    * Added Apple ID sign in Prototype
    
    * Added apple sign in

--- AquaGrowthMobile/Views/login_view.swift
+++ AquaGrowthMobile/Views/login_view.swift
@@ -11,11 +11,15 @@ import GoogleSignIn
 import GoogleSignInSwift
 import FirebaseAuth
 
+import Firebase
+import AuthenticationServices
+import CryptoKit
 struct LoginView: View {
     //Viewmodels for Google and Login Backend
     @StateObject var google_viewModel = signIn_google_viewModel()
     @StateObject var viewModel = login_viewmodel()
 
+    @State private var nonce: String?
     //Color scheme for dark and light mode
     @Environment(\.colorScheme) var colorScheme
 
@@ -94,8 +98,29 @@ struct LoginView: View {
                             .clipShape(Circle()) // Set the button into a circle shape. It was originally a rectangle.
                             .shadow(radius: 4) // Adds a shadow to the button.
                         }
-                            .padding(.bottom, 20) //Set padding below the Google Icon
+                        .padding(.bottom, 20) //Set padding below the Google Icon
                         
+                        //Adding Sign in With Apple Button Here
+                            
+                        SignInWithAppleButton(.signIn) { request in
+                            
+                            let nonce = randomNonceString()
+                            self.nonce = nonce
+                            request.requestedScopes = [.email, .fullName]
+                            request.nonce = sha256(nonce)
+                        } onCompletion: { result in
+                            switch result {
+                            case .success(let authorization):
+                                signInWithApple(authorization)
+                            case .failure(let error):
+                                print(error.localizedDescription)
+                            }
+                            // Do something
+                        }
+                        .frame(width: 200, height: 45)
+                        .clipShape(.capsule)
+                        .padding(.top, 10)
+
                         NavigationLink{
                             RegisterView()
                                 .navigationBarBackButtonHidden(true)
@@ -110,6 +135,67 @@ struct LoginView: View {
                 }
             }
         }
+    }
+
+    func signInWithApple(_ authorization: ASAuthorization ){
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce else {
+                print("Invalid state: A login callback was received, but no login request was sent.")
+                return
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential, including the user's full name.
+            let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
+                                                           rawNonce: nonce,
+                                                           fullName: appleIDCredential.fullName)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error.localizedDescription)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+            }
+        }
+    }
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+        
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        
+        let nonce = randomBytes.map { byte in
+            // Pick a random character from the set, wrapping around if needed.
+            charset[Int(byte) % charset.count]
+        }
+        
+        return String(nonce)
+    }
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
     }
 }
 
