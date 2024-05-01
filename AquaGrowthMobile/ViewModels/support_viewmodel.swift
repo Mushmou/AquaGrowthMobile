b2e5ac3563274462c6b6d5c9da9dc5fa0397d6b0
Megan Kang <80795330+MeganKang@users.noreply.github.com>
2024-05-01 10:44:17 -0700

    Support page setup

---
+++ AquaGrowthMobile/ViewModels/support_viewmodel.swift
@@ -0,0 +1,18 @@
+//
+//  support_viewmodel.swift
+//  AquaGrowthMobile
+//
+//  Created by Megan Kang on 5/1/24.
+//
+
+import Foundation
+import UIKit
+
+class support_viewmodel{
+    
+    func sendFeedback(_ message: String, completion: @escaping (Bool) -> Void){
+        
+        completion(true)
+    }
+}
+
---
+++ AquaGrowthMobile/Views/support_view.swift
@@ -0,0 +1,13 @@
+//
+//  support_view.swift
+//  AquaGrowthMobile
+//
+//  Created by Megan Kang on 5/1/24.
+//
+
+import Foundation
+import UIKit
+
+class support_view: UIViewController{
+    
+}
