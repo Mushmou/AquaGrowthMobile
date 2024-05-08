9e285bb044666a8474371af6882d5ff621db6e7f
Megan Kang <80795330+MeganKang@users.noreply.github.com>
2024-05-07 18:57:33 -0700

    Formatting Support Page

--- AquaGrowthMobile/Views/support_view.swift
+++ AquaGrowthMobile/Views/support_view.swift
@@ -11,59 +11,81 @@ import MessageUI
 struct SupportView: View {
     @State private var messageText = ""
     @StateObject private var viewModel = support_viewmodel()
+    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+    @Environment(\.colorScheme) var colorScheme
+    @State private var showNavigationBar = true
 
     var body: some View {
-        VStack {
-//            Rectangle()
-//                .fill(Color(red: 0.28, green: 0.59, blue: 0.17))
-//                .frame(width: UIScreen.main.bounds.width, height: 500) // Change the size of the VStack
-//                .position(x: UIScreen.main.bounds.width / 2.179, y: 10)
-//            
-////            Rectangle()
-////                .fill(.white)
-////                .frame(width: UIScreen.main.bounds.width, height: 640) // Change the size of the VStack
-////                .position(x: UIScreen.main.bounds.width / 2, y: 550)
-            
-//            Text("Help & Support")
-//                .font(.system(size: 45))
-//                .bold()
-//                .padding(.top, 20)
-//                .position(x: UIScreen.main.bounds.width / 2.179, y: -25)
-//                .foregroundColor(.white)
-            Text("Help and Support")
-                .bold()
-                .font(.largeTitle)
-                .padding(.top, 20)
-            
-            Text("Have a question? Send us a message.")
-                .font(.headline)
-                .padding(.top, 20)
-            
-            TextEditor(text: $messageText)
-                .frame(height: 150)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
+        VStack{
+            ZStack{
+                Color.clear.frame(width: 1, height:1)
+                    .navigationBarBackButtonHidden(true)
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button (action: {
+                                presentationMode.wrappedValue.dismiss()
+                            }, label: {
+                                Image(systemName: "chevron.backward")
+                                    .font(.system(size: 30)) // Adjust the size as needed
+                                    .foregroundColor(colorScheme == .dark ? .white : .black)
+                            })
+                            .onTapGesture {
+                                withAnimation {
+                                    showNavigationBar.toggle()
+                                }
+                            }
+                        }
+                    }
+                VStack{
+                    Rectangle()
+                        .fill(Color(red: 0.28, green: 0.59, blue: 0.17))
+                        .frame(maxWidth: .infinity, maxHeight: 300)
+                    
+                    Spacer(minLength: 0)
+                    
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(maxWidth: .infinity)
+                        .border(.red)
+                }
+                VStack {
+                    Text("Help and Support")
+                        .bold()
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle)
+                        .padding(.bottom, 100)
+                    
+                    Text("Have a question? Send us a message.")
+                        .font(.headline)
+                        .padding(.top, 20)
+                    
+                    TextEditor(text: $messageText)
+                        .frame(height: 150)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    
+                    Button(action: {
+                        sendMessage()
+                    }, label: {
+                        Text("Send")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color(red: 0.28, green: 0.59, blue: 0.17))
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    })
+                    .padding(.top, 20)
+                }
                 .padding(.horizontal)
-            
-            Button(action: {
-                sendMessage()
-            }, label: {
-                Text("Send")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color(red: 0.28, green: 0.59, blue: 0.17))
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            })
-            .padding(.top, 20)
-        }
-        .padding(.horizontal)
-        .navigationBarTitle("Support", displayMode: .inline)
-        .alert(isPresented: $viewModel.showingMailAlert) {
-            Alert(title: Text(viewModel.mailAlertTitle), message: Text(viewModel.mailAlertMessage), dismissButton: .default(Text("OK")))
+                .navigationBarTitle("Support", displayMode: .inline)
+                .alert(isPresented: $viewModel.showingMailAlert) {
+                    Alert(title: Text(viewModel.mailAlertTitle), message: Text(viewModel.mailAlertMessage), dismissButton: .default(Text("OK")))
+                }
+            }.edgesIgnoringSafeArea(.all)
         }
     }
 
@@ -75,3 +97,4 @@ struct SupportView: View {
 #Preview {
     SupportView()
 }
+
